0e60f9f075c527bbc823251d9805bce8c985d388
nh-polash <polashhoque0@gmail.com>
2021-11-08 21:46:59 +0600

    Al Kemi Hospital site done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header/Header';
+import AuthProvider from './context/AuthProvider';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Footer from './components/Footer/Footer';
+import Services from './components/Home/Services/Services';
+import Errorfile from './components/Errorfile/Errorfile';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Information from './components/Information/Information';
+import Doctors from './components/Home/Doctors/Doctors';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/register">
+            <Register></Register>
+          </Route>
+          <Route path="/services">
+            <Services></Services>
+          </Route>
+          {/* <Route path="/about">
+            <About></About>
+          </Route> */}
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+          <Route path="/doctors">
+            <Doctors></Doctors>
+          </Route>
+          <PrivateRoute path="/information/:serviceId">
+            <Information></Information>
+          </PrivateRoute>
+          <PrivateRoute path="/about">
+            <About></About>
+          </PrivateRoute>
+          <Route exact path="*">
+            <Errorfile></Errorfile>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+        </BrowserRouter>
+      </AuthProvider>
+  
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA4M0BL2oG43sO_pRpHiu4M0peNdvwlW9o",
+  authDomain: "al-kemi-hospital.firebaseapp.com",
+  projectId: "al-kemi-hospital",
+  storageBucket: "al-kemi-hospital.appspot.com",
+  messagingSenderId: "709683763259",
+  appId: "1:709683763259:web:1a231ec9718827ad02a41e",
+  measurementId: "G-K6SGCGTVMS"
+};
+
+  export default firebaseConfig;
